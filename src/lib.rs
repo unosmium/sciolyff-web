d22d1fccfd43cce45eb80e58934a5c463422fa9b
Em Zhan <zqianem@gmail.com>
2020-06-30 10:05:05 -0400

    Update crates and sciolyff options

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
-use sciolyff::interpreter::Interpreter;
+use sciolyff::interpreter::{html::HTMLOptions, Interpreter};
+use std::default::Default;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn generate_results(sciolyff: &str, hide_raw: bool, color: &str) -> String {
-    Interpreter::from_yaml(sciolyff).to_html(hide_raw, color)
+    Interpreter::from_yaml(sciolyff).to_html(&HTMLOptions {
+        hide_raw,
+        color: color.to_string(),
+        ..Default::default()
+    })
 }
